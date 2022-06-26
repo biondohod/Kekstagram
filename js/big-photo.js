3064ff932267435aa83c17b83a7ef47a351de353
Andrey <biondohod@gmail.com>
2022-06-26 15:45:02 +0200

    Изменяет скрипты для удаления обработчиков события при закрытии попапа

--- js/big-photo.js
+++ js/big-photo.js
@@ -20,40 +20,53 @@ const createCommentsList = function(commentsList) {
 
 };
 
-const addPreviewListener = function(photo, description, comments) {
-  photo.addEventListener('click', function() {
-    let bigPictureImg = bigPicture.querySelector('.big-picture__img').querySelector('img')
-    bigPictureImg.src = photo.querySelector('.picture__img').src;
-    bigPictureImg.alt = description;
+const renderBigPhoto = (photo, description, comments) => {
+  let bigPictureImg = bigPicture.querySelector('.big-picture__img').querySelector('img')
+  bigPictureImg.src = photo.querySelector('.picture__img').src;
+  bigPictureImg.alt = description;
 
-    let bigPictureDescription = bigPicture.querySelector('.social__caption');
-    bigPictureDescription.textContent = description;
+  let bigPictureDescription = bigPicture.querySelector('.social__caption');
+  bigPictureDescription.textContent = description;
 
-    let bigPictureLikes = bigPicture.querySelector('.likes-count');
-    bigPictureLikes.textContent = photo.querySelector('.picture__likes').textContent;
+  let bigPictureLikes = bigPicture.querySelector('.likes-count');
+  bigPictureLikes.textContent = photo.querySelector('.picture__likes').textContent;
 
-    let bigPictureCommentsCount = bigPicture.querySelector('.comments-count');
-    bigPictureCommentsCount.textContent = photo.querySelector('.picture__comments').textContent;
+  let bigPictureCommentsCount = bigPicture.querySelector('.comments-count');
+  bigPictureCommentsCount.textContent = photo.querySelector('.picture__comments').textContent;
 
-    createCommentsList(comments);
-
-    document.querySelector('.social__comment-count').classList.add('hidden');
-    document.querySelector('.comments-loader').classList.add('hidden');
-    document.body.classList.add('modal-open');
-    bigPicture.classList.remove('hidden');
-  });
+  createCommentsList(comments);
 };
 
-buttonClose.addEventListener('click', function() {
+function openBigPhoto(photo, description, comments) {
+  renderBigPhoto(photo, description, comments);
+  document.querySelector('.social__comment-count').classList.add('hidden');
+  document.querySelector('.comments-loader').classList.add('hidden');
+  document.body.classList.add('modal-open');
+  bigPicture.classList.remove('hidden');
+  document.addEventListener('keydown', onBigPhotoEscKeydown);
+  buttonClose.addEventListener('click', closeBigPhoto);
+
+};
+
+const onBigPhotoEscKeydown = (evt) => {
+  if (evt.key === 'Escape') {
+    evt.PreventDefault;
+    closeBigPhoto();
+  }
+};
+
+function closeBigPhoto() {
   bigPicture.classList.add('hidden');
   document.body.classList.remove('modal-open');
-});
+  buttonClose.removeEventListener('click', closeBigPhoto);
+  document.removeEventListener('keydown', onBigPhotoEscKeydown);
+}
 
-document.addEventListener('keydown', function(evt) {
-  if (evt.keyCode == 27) {
-    bigPicture.classList.add('hidden');
-    document.body.classList.remove('modal-open');
-  }
-});
+const addPreviewListener = function(photo, description, comments) {
+  photo.addEventListener('click', () => {
+    openBigPhoto(photo, description, comments);
+  })};
+
+
 
 export{addPreviewListener};
